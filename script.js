71aaef7b55c1742fbee13fe721fc6d2d3019f988
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-01-16 15:23:43 +0530

    Accessed bookmark list element using document.getElementById

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const bookMarkForm = document.getElementById("bookmarkForm");
 const siteNameInput = document.getElementById("siteName");
 const siteUrlInput = document.getElementById("siteUrl");
+const bookmarkList = document.getElementById("bookmarkList");
 
