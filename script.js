9bd642725c477b0635607d985face5bacb06da74
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-01-17 15:01:35 +0530

    Defined callback to exclude the bookmark to be removed

--- script.js
+++ script.js
@@ -100,6 +100,8 @@ bookmark.name.toLowerCase() === siteName.toLowerCase() && bookmark.url.toLowerCa
 function removeBookmark(bookmark){
     let bookmarks = getBookmarks();
     bookmarks = bookmarks.filter(
+    (b) =>
+
 
 
 
