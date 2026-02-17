15fd7fa7e0c2c65889a080c871b5b9dabede9219
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-17 18:20:20 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -6,107 +6,112 @@ const bookmarkList = document.getElementById("bookmarkList");
 bookMarkForm.addEventListener("submit",addBookmark);
 document.addEventListener("DOMContentLoaded",displayBoomarks);
 
-
 function addBookmark(event){
     event.preventDefault();
-    
+
     const siteName = siteNameInput.value;
     const siteUrl = siteUrlInput.value;
 
-if(!validateForm(siteName,siteUrl)){
-            return;
+    if(!validateForm(siteName,siteUrl)){
+        return;
     }
 
-if(isDuplicateBookmark(siteName,siteUrl)){
-alert("This bookmark already exists");
-            return;
+    if(isDuplicateBookmark(siteName,siteUrl)){
+        alert("This bookmark already exists");
+        return;
     }
 
-const bookmark = {
-      name:siteName,
+
+    const bookmark = {
+        name:siteName,
         url: siteUrl,
-};
+    };
 
-saveBookmark(bookmark);
+    saveBookmark(bookmark);
 
-displayBookmark(bookmark);
-    
+    displayBookmark(bookmark);
+
     siteNameInput.value = ""
     siteUrlInput.value = ""
 
 }
 
 function validateForm(siteName,siteUrl){
-if(!siteName || !siteUrl){
-alert("Please fill in both fields.")
-return;
-}
+    if(!siteName || !siteUrl){
+        alert("Please fill in both fields.")
+        return;
+    }
 
-try{
-    new URL(siteUrl);
-    return true;
-}catch(error){
-alert("Please enter a valid URL.")
-            return false;
+    try{
+        new URL(siteUrl);
+        return true;
+    }catch(error){
+        alert("Please enter a valid URL.")
+        return false;
     }
 }
 
 function displayBookmark(bookmark){
-const bookmarkItem = document.createElement("li");
-const link = document.createElement("a");
-link.href = bookmark.url;
-link.textContent = bookmark.name;
-link.target = "_blank";
+    const bookmarkItem = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = bookmark.url;
+    link.textContent = bookmark.name;
+    link.target = "_blank";
 
+        // removal Items
+    const removeButton = document.createElement("button");
+    removeButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
 
-const removeButton = document.createElement("button");
-removeButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    removeButton.addEventListener("click",()=> removeBookmark(bookmark));
 
-removeButton.addEventListener("click",()=> removeBookmark(bookmark));
     bookmarkItem.appendChild(link);
     bookmarkItem.appendChild(removeButton);
     bookmarkList.appendChild(bookmarkItem);
 }
 
-
 function saveBookmark(bookmark){
     let bookmarks = getBookmarks();
-        bookmarks.push(bookmark);
-        localStorage.setItem("bookmarks",JSON.stringify(bookmarks));
+    bookmarks.push(bookmark);
+    localStorage.setItem("bookmarks",JSON.stringify(bookmarks));
 }
 
 function getBookmarks(){
-        return JSON.parse(localStorage.getItem("bookmarks")) || [];
+
+    return JSON.parse(localStorage.getItem("bookmarks")) || [];
 
 }
 
 function displayBoomarks(){
-        let bookmarks = getBookmarks();
-        bookmarks.forEach((bookmark)=>{
-                    displayBookmark(bookmark);
+    let bookmarks = getBookmarks();
+    bookmarks.forEach((bookmark)=>{
+        displayBookmark(bookmark);
     })
-}
 
+}
 
 function isDuplicateBookmark(siteName,siteUrl){
-        let bookmarks = getBookmarks();
-        return bookmarks.some(
+
+    let bookmarks = getBookmarks();
+
+    return bookmarks.some(
         (bookmark) =>
-bookmark.name.toLowerCase() === siteName.toLowerCase() && bookmark.url.toLowerCase() === siteUrl.toLowerCase()
+            bookmark.name.toLowerCase() === siteName.toLowerCase() && bookmark.url.toLowerCase() === siteUrl.toLowerCase()
     );
 
 }
 
+
 function removeBookmark(bookmark){
+
     let bookmarks = getBookmarks();
     bookmarks = bookmarks.filter(
-    (b) =>
-    b.name.toLowerCase() !== bookmark.name.toLowerCase() || 
-                b.url.toLowerCase() !== bookmark.url.toLowerCase()
+        (b) =>
+        b.name.toLowerCase() !== bookmark.name.toLowerCase() || 
+        b.url.toLowerCase() !== bookmark.url.toLowerCase()
     );
 
-localStorage.setItem("bookmarks",JSON.stringify(bookmarks));
-        clearBookmarkList();
+    localStorage.setItem("bookmarks",JSON.stringify(bookmarks));
+    clearBookmarkList();
     displayBoomarks();
 
 }
@@ -114,10 +119,3 @@ localStorage.setItem("bookmarks",JSON.stringify(bookmarks));
 function clearBookmarkList(){
     bookmarkList.innerHTML = "";
 }
-
-
-
-
-
-
-    
